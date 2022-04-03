adc03ee45544b1f557e5af37017e97af0c20fb94
Alexandre Fonseca <alexandre.fonseca@live.com>
2022-04-03 11:49:03 -0300

    first deploy

--- src/controllers/ConceitoController.js
+++ src/controllers/ConceitoController.js
@@ -1,16 +1,49 @@
 const Conceito = require('../models/Conceitos');
 
-
 class ConceitoController {
     async store(req, res) {
         let {
             verbete, conceito, experiencia, referencia, npat
         } = (req.body);
-
         await Conceito.create({
             verbete, conceito, experiencia, referencia, npat
         });
         return res.json({ verbete, conceito, experiencia, referencia, npat })
+    }
+    async indexon(req, res) {
+        const conceito = await Conceito.findById({ _id: req.params.id });
+
+        if (conceito) {
+            return res.status(200).json(conceito);
+        }
+        else {
+            return res.status(400).json({ mensagem: "conceito não encontrado" })
+        }
+    }
+    async index(req, res) {
+        const conceito = await Conceito.find().sort({ verbete: 1 });
+
+        if (conceito) {
+            return res.status(200).json(conceito);
+        }
+        else {
+            return res.status(400).json({ mensagem: "não existe informação para essa consulta" })
+        }
+    }
+    async indexvb(req, res) {
+        const conceito = await Conceito.find({ verbete: req.params.vb });
+
+        if (conceito) {
+            return res.status(200).json(conceito);
+        }
+        else {
+            return res.status(400).json({ mensagem: "não existe informação para essa consulta" })
+        }
+    }
+    async indeall(req, res) {
+
+        await Conceito.remove({});
+        return res.status(200).json({ msg: "Todos os registros foram apagados" });
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,9 @@ const ConceitoController = require('../controllers/ConceitoController');
 const Router = express.Router();
 
 Router.post("/api/conceito/", ConceitoController.store);
+Router.get("/api/conceito/:id", ConceitoController.indexon);
+Router.get("/api/conceito/", ConceitoController.index);
+Router.get("/api/conceito/vb/:vb", ConceitoController.indexvb);
+Router.delete("/api/conceito/", ConceitoController.indeall);
 
 module.exports = Router;

ec449ba13c85eee11d96d598a3cf40d2fa03afff
Alexandre Fonseca <54107462+alexandrefonseca1972@users.noreply.github.com>
2022-04-06 06:41:56 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,6 @@ Router.get("/api/conceito/:id", ConceitoController.indexon);
 Router.get("/api/conceito/", ConceitoController.index);
 Router.get("/api/conceito/vb/:vb", ConceitoController.indexvb);
 Router.delete("/api/conceito/", ConceitoController.indeall);
+Router.put("/api/conceito/:id", ConceitoController.update);
 
 module.exports = Router;
